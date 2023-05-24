37b9b60cca2a17e66e81fcc5b66f89a148174bbf
Hulkle83 <134441597+Hulkle83@users.noreply.github.com>
2023-05-24 10:19:43 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,6 @@ self.addEventListener('install', event => {
     event.waitUntil((async () => {
         const cache = await caches.open(CACHE_NAME);
         cache.addAll([
-            '/',
             'js/pdfootsteps.js',
             'css/pdfootsteps.css'
         ]);
